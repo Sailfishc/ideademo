72f2a066cad9aa96981e4f469ef093e0eee5ad8c
sailfishc <mr.cheng94@foxmail.com>
2016-12-10 14:20:04 +0800

    cheng-main

--- src/main/java/com/sailfish/JavaDemo.java
+++ src/main/java/com/sailfish/JavaDemo.java
@@ -4,5 +4,7 @@ package com.sailfish;
  * Created by travis on 2016/12/10.
  */
 public class JavaDemo {
-
+    public static void main(String[] args) {
+        System.out.println("hello idea!");
+    }
 }
